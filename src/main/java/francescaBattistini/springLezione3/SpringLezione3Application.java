3baeb6c6fa53bb5097bca184d9844ed4f04791ee
Francescabattistini <166622198+Francescabattistini@users.noreply.github.com>
2024-10-24 15:54:07 +0200

    Initial commit

---
+++ src/main/java/francescaBattistini/springLezione3/SpringLezione3Application.java
@@ -0,0 +1,13 @@
+package francescaBattistini.springLezione3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLezione3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLezione3Application.class, args);
+	}
+
+}
---
+++ src/test/java/francescaBattistini/springLezione3/SpringLezione3ApplicationTests.java
@@ -0,0 +1,13 @@
+package francescaBattistini.springLezione3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLezione3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
